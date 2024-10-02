3b23f97d95fd89fb7d9a0ec2e503c51720f8e962
Intercalaire <vgodart@1F4.42angouleme.fr>
2024-10-02 08:07:52 +0200

    debut CPP00

---
+++ CPP/CPP00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vgodart <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/23 04:49:59 by vgodart           #+#    #+#             */
+/*   Updated: 2024/08/23 04:50:02 by vgodart          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+
+	j = -1;
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (argc > 1 && argv && argv[++j])
+	{
+		i = -1;
+		while (j > 0 && argv[j][++i])
+			std::cout << (char)std::toupper(argv[j][i]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
